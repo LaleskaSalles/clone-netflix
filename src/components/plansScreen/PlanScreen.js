f9c0380e6c79f2dbd939fc39dbc8258c876c0c20
Laleska <laleskasalomao@gmail.com>
2023-10-03 22:34:46 -0300

    rediret plans

--- src/components/plansScreen/PlanScreen.js
+++ src/components/plansScreen/PlanScreen.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from "../../features/userSlice"
 import './PlanScreen.css'
-import db from '../../firebase';
-import { collection, query, where, getDocs } from "firebase/firestore";
-
+import db, { auth } from '../../firebase';
+import { collection, query, where, getDocs, onSnapshot, addDoc } from "firebase/firestore";
+import { loadStripe } from '@stripe/stripe-js';
 
 export default function PlanScreen() {
     const [products, setProducts] = useState([])
+    const user = useSelector(selectUser);
+
 
     useEffect(() => {
         const getProducts = async () => {
@@ -37,21 +41,54 @@ export default function PlanScreen() {
 
     console.log(products);
 
+
+    const loadCheckout = async (priceId) => {
+        const currentUser = auth.currentUser;
+
+        if (currentUser ) {
+
+            await addDoc(collection(db, 'customers', user.uid, 'checkout_sessions'), {
+                price: priceId,
+                success_url: window.location.origin,
+                cancel_url: window.location.origin
+            });
+
+            onSnapshot(collection(db, 'customers', user.uid, 'checkout_sessions'), (snapshot) => {
+                snapshot.docs.forEach(async (doc) => {
+                  const { error, sessionId } = doc.data();
+            
+                  if (error) {
+                    alert(`An error occurred: ${error.message}`);
+                  }
+            
+                  if (sessionId) {
+                    // Redirecionar para o Checkout
+                    const stripe = await loadStripe('pk_test_51NrNjOCfEOrmhZdAs1xQ3Bvadw7kY26md2J3yJzvdPW67rxJR69ej5hkSfpUrh7I9vZz6J0vpgpryVjbXq4wUKWG00lH2swqXr');
+                    stripe.redirectToCheckout({ sessionId });
+                  }
+                });
+              });
+            } 
+            
+            };
+
     return (
         <div className='plansScreen'>
             {Object.entries(products).map(([productId, productData]) => {
                 // logic to check if the user's subscription is active...
-                return(
-                    <div className='plansScreen_plan'> 
+                return (
+                    <div key={productId} className='plansScreen_plan'>
                         <div className='plansSreen_info'>
                             <h5>{productData.name}</h5>
                             <h6>{productData.description}</h6>
-                        </div>                  
-                        <button>Subscribe</button>          
+                        </div>
+                        <button onClick={() => loadCheckout(productData.prices[0].priceId)}>Subscribe</button>
                     </div>
                 )
             })}
 
         </div>
     )
-}
+
+
+ }
