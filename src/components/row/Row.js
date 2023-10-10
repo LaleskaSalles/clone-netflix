46f934d5af01afd03af2d2e94640b797e22dadd3
Laleska <laleskasalomao@gmail.com>
2023-10-09 21:15:01 -0300

    layout_erros

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react'
 import './Row.css'
 import axios from "../../axios"
+import { MdOutlineNavigateBefore, MdOutlineNavigateNext } from "react-icons/md";
+import { useRef } from 'react';
 
 export default function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
-
     const base_url = "https://image.tmdb.org/t/p/original/";
+
+
+    const rowRef = useRef(null)
+
+    const handleLeftArrow = () => {
+        rowRef.current.scrollLeft -= 400;
+    };
+  
+    const handleRighttArrow = () => {
+              rowRef.current.scrollLeft += 400;
+    };
 
     useEffect(() => {
         async function fetchData() {
@@ -20,7 +32,15 @@ export default function Row({ title, fetchUrl, isLargeRow = false }) {
         <div className='row'>
             <h2>{title}</h2>
 
-            <div className='row_posters'>
+            <div className='rowMovies--left' onClick={handleLeftArrow}>
+                <MdOutlineNavigateBefore/>
+            </div>
+            <div className='rowMovies--right' onClick={handleRighttArrow}>
+                <MdOutlineNavigateNext/>
+            </div>
+
+            <div className='row_posters' ref={rowRef}>
+
                 {movies.map((movie) =>
                     ((isLargeRow && movie.backdrop_path) || (!isLargeRow && movie.backdrop_path)) &&
                     (
@@ -35,6 +55,9 @@ export default function Row({ title, fetchUrl, isLargeRow = false }) {
                     )
                 )}
             </div>
+
+            
+
         </div>
     )
 }
--- src/components/signUp/SignUpScreen.js
+++ src/components/signUp/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import "./SignUpScreen.css";
 import { auth } from '../../firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
@@ -9,49 +9,103 @@ export default function SignUpScreen() {
   const navegate = useNavigate();
   const emailRef = useRef(null)
   const passwordRef = useRef(null);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
-  const register = (e) =>{
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  };
+
+  const register = (e) => {
     e.preventDefault();
+    clearErrors();
+
+    if (!emailRef.current.value && !passwordRef.current.value) {
+      setEmailError('Required field');
+      setPasswordError('Required field');
+      return;
+    } if(!emailRef.current.value){
+      setEmailError('Required field');
+      return;
+    } if(!passwordRef.current.value){
+      setPasswordError('Required field');
+      return;
+    }
 
     createUserWithEmailAndPassword(
       auth,
       emailRef.current.value,
       passwordRef.current.value
     )
-    .then((authUser) => {
-      console.log(authUser);
-    })
-    .catch((error) => {
-      alert(error.message)
-    });
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-email") {
+          setEmailError("Invalid email");
+        }else if (error.code === "auth/weak-password") {
+          setPasswordError("The password must be at least 6 characters long.");
+        } else {
+          alert(error.message);
+        }
+      });
   };
 
   const signIn = (e) => {
     e.preventDefault();
+    clearErrors();
+
+    if (!emailRef.current.value && !passwordRef.current.value) {
+      setEmailError('Required field');
+      setPasswordError('Required field');
+      return;
+    } if(!emailRef.current.value){
+      setEmailError('Required field');
+      return;
+    } if(!passwordRef.current.value){
+      setPasswordError('Required field');
+      return;
+    }
+
 
     signInWithEmailAndPassword(
       auth,
       emailRef.current.value,
       passwordRef.current.value
     )
-    .then((authUser) => {
-      console.log(authUser);
-      navegate('/');
-    })
-    .catch((error) => {
-      alert(error.message)
-    });
+      .then((authUser) => {
+        console.log(authUser);
+        navegate('/');
+      })
+      .catch((error) => {
+        if (error.code === "auth/invalid-email" || error.code === "auth/wrong-password") {
+          setEmailError("Incorrect email or password");
+          setPasswordError("Incorrect email or password");
+        } else if (error.code === "auth/user-not-found") {
+          setEmailError("User not found, sign up")
+        } else {
+          alert(error.message);
+        }
+      });
   }
 
   return (
     <div className='signUpScreen'>
-        <form>
-            <h1>Sign In</h1>
-            <input ref={emailRef} required={true} placeholder='Email' type='email'/>
-            <input ref={passwordRef} required={true} placeholder='Password' type='password'/>
-            <button className='signIn' type='submit' onClick={signIn}>Sign In</button>
-            <button className='signUp' type='submit' onClick={register}>Sign Up</button>
-        </form>
+      <form>
+        <h1>Sign In</h1>
+        <input ref={emailRef} placeholder='Email' type='email' onChange={clearErrors} />
+        <span className="error">{emailError}</span>
+
+        <input ref={passwordRef} placeholder='Password' type='password' onChange={clearErrors} />
+        <span className="error">{passwordError}</span>
+
+        <button className='signIn' type='submit' onClick={signIn}>Sign In</button>
+        <h4>
+          <span className='signUpScreen_gray'>New to Netflix? </span>
+        </h4>
+        <button className='signUp' type='submit' onClick={register}>Sign Up</button>
+      </form>
     </div>
   )
 }
